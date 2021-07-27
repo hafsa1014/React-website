c9884bf3fd86f1c74578143e7c81fee2f759c4e3
Hafsa Sahin <hafshasahin14@gmail.com>
2021-07-27 20:18:02 +0530

    Update Common.jsx

--- src/Common.jsx
+++ src/Common.jsx
@@ -12,7 +12,7 @@ const Common = (props) => {
                             <div className="col-md-6 p-t-5 pt-lg-0 order-2 order-lg-1 display-flex justify-content-center flex-column">
                                 <h1>
                                   {props.name}
-                                   <strong className="brand-name">Thapa Technical</strong>
+                                   <strong className="brand-name">Dream Service</strong>
                                 </h1>
                                 <h2 className="my-3">
                                     We are the team of talented developer making websites
